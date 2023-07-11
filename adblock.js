183c03574ec1f75606a5f550c28520b385a45c8a
linknprk <98031434+linknprk@users.noreply.github.com>
2023-07-11 19:18:33 -0400

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -2,6 +2,6 @@ var popup = window.open("", "_blank");
 if(popup == null || popup === undefined){
   document.write("<h1>Disable your adblocker!</h1>");
 }else{
-  document.write("<p>If your adblocker is on please turn it off");
+  document.write("<p>If your adblocker is on please turn it off</p>");
   window.open("https://pytutorials.github.io", "_blank");
 }
