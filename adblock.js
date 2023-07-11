bcea9a8c0e62b59f22ed79a15585e4ce2bcb065e
linknprk <98031434+linknprk@users.noreply.github.com>
2023-07-11 19:26:31 -0400

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -1,5 +1,6 @@
 var popup = window.open("", "_blank");
 if(popup == null || popup === undefined){
+  document.body.style.display = "none"; 
   document.write("<h1>Disable your adblocker!</h1>");
 }else{
   document.write("<p>If your adblocker is on please turn it off</p>");
