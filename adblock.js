c0a09209a6eb2b28e253e40501ba79e71a7f6e02
linknprk <98031434+linknprk@users.noreply.github.com>
2023-07-12 14:26:28 -0400

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -3,6 +3,3 @@ if(!popup == null || popup != undefined){
   window.open("https://pytutorials.github.io/", "_blank");
   popup.close();
 }
-if(popup == null || popup == undefined){
-  alert"If you feel like disabling your adblocker please do");
-}
